e94bc81bc635fb7c2366c1e08870e5424c98cf54
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-09-04 17:09:31 +0600

    get request for all products

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Techlog server is running');
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ejpclfg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("techlog").collection("productCollection");
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+
+        app.get('/product/:categoryId', async (req, res) => {
+            const category = req.params.categoryId;
+            const query = { category: category };
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+    }
+    finally { }
+}
+run().catch(console.dir)
+
+app.listen(port, () => {
+    console.log(`App is  running on port ${port}`)
+})
